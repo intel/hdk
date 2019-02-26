intel/hdk

commit a18e1dcf1d1672856b36ec37d6d2997897ee4e40

Fixing assertion failure when query falls back to cpu when a subquery ran in gpu mode (#318)
* Fixing assertion failure when query falls back to cpu when a subquery ran in gpu mode

* Re-adding check to make sure context_data_ has been cleaned up

omniscidb/QueryEngine/RelAlgExecutor.cpp
@@ -370,7 +370,7 @@ ExecutionResult RelAlgExecutor::executeRelAlgSeq(std::vector<RaExecutionDesc>& e
   // subqueries are available throughout the execution of the sequence.
   for (auto subquery : subqueries_) {
     auto temp_table = subquery->getExecutionResult();
-    if (temp_table.get()) {
+    if (temp_table.get() && temporary_tables_.find(-(subquery->getRelAlg()->getId())) == temporary_tables_.end()) {
       addTemporaryTable(-(subquery->getRelAlg()->getId()), temp_table->getDataPtr());
     }
   }

commit 562b9db321b5b1695864ea27dccdfa61a48b24ab

Fix minor formatting issue in RelAlgExecutor.cpp
Introduced in a18e1dcf1d1672856b36ec37d6d2997897ee4e40, missed by a
misconfigured Jenkins job.

omniscidb/QueryEngine/RelAlgExecutor.cpp
@@ -371,7 +371,8 @@ ExecutionResult RelAlgExecutor::executeRelAlgSeq(std::vector<RaExecutionDesc>& e
   // subqueries are available throughout the execution of the sequence.
   for (auto subquery : subqueries_) {
     auto temp_table = subquery->getExecutionResult();
-    if (temp_table.get() && temporary_tables_.find(-(subquery->getRelAlg()->getId())) == temporary_tables_.end()) {
+    if (temp_table.get() && temporary_tables_.find(-(subquery->getRelAlg()->getId())) ==
+                                temporary_tables_.end()) {
       addTemporaryTable(-(subquery->getRelAlg()->getId()), temp_table->getDataPtr());
     }
   }