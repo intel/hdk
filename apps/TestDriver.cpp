aa7f759dff50a3cf72da22ff04b9e114aab960b6
Alex Baden <alexander.baden@intel.com>
2022-02-08 16:30:30 -0700

    Bring QueryEngine into HDK 1/?

--- apps/TestDriver.cpp
+++ apps/TestDriver.cpp
@@ -9,5 +9,8 @@ int main(void) {
 
   const auto col_expr =
       makeExpr<Analyzer::ColumnVar>(sql_type, table_id, /*column_id=*/0, 0);
+
+  const auto work_unit = hdk::create_work_unit();
+
   std::cout << "Test program worked: " << col_expr->toString() << std::endl;
 }
--- src/HDK.h
+++ src/HDK.h
@@ -1,1 +1,11 @@
 #include "Expression/Expression.h"
+
+#include "../omniscidb/QueryEngine/RelAlgExecutionUnit.h"
+
+namespace hdk {
+
+struct WorkUnit {};
+
+WorkUnit create_work_unit();
+
+}  // namespace hdk
