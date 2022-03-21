ef9b4bedf81a68a3f806a4bf62c6a0eee70278a0
Alex Baden <alex.baden@gmail.com>
2022-03-21 11:00:49 -0600

    MLIR Skeleton

---
+++ src/mlir/Ops.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "mlir/IR/BuiltinTypes.h"
+#include "mlir/IR/Dialect.h"
+#include "mlir/IR/OpDefinition.h"
+#include "mlir/Interfaces/SideEffectInterfaces.h"
+
+#define GET_OP_CLASSES
+#include "Ops.h.inc"
+
