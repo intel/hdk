e3eafb80e60312517c099938396d52f8b4c3660f
Alex Baden <alex.baden@gmail.com>
2022-03-24 12:16:04 -0600

    HDK MLIR Dialect 2/?
    (build broken)

--- src/HDK.cpp
+++ src/HDK.cpp
@@ -2,8 +2,25 @@
 
 #include <iostream>
 
+#include "AST/AST.h"
+#include "MLIRGen.h"
+
 #include "mlir/Dialect.h"
 
 void mlir_test() {
   std::cout << "### Testing MLIR Dialect ###" << std::endl;
+
+  hdk::AST::KernelSequence sequence;
+  auto kernel = hdk::AST::Kernel();
+  sequence.emplace_back(kernel);
+
+  mlir::MLIRContext context;
+  // Load our Dialect in this MLIR Context.
+  context.getOrLoadDialect<hdk::HDKDialect>();
+  auto module = hdk::mlirGen(context, sequence);
+  if (!module) {
+    std::cerr << "Failed to generate MLIR module!" << std::endl;
+  } else {
+    module->dump();
+  }
 }
