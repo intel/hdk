8d0268c42a5422b2b32df6922bad40adb99e8e3b
Alex Baden <alex.baden@gmail.com>
2022-03-24 12:16:04 -0600

    HDK MLIR Dialect 2/?
    (build broken)

--- src/mlir/Dialect.cpp
+++ src/mlir/Dialect.cpp
@@ -1,10 +1,26 @@
 #include "mlir/Dialect.h"
 #include "mlir/Ops.h"
 
+#include "mlir/IR/Dialect.h"
+
+#include "Dialect.cpp.inc"
+
 namespace hdk {
 
 void HDKDialect::initialize() {
-  addOperations<hdk::ConstantOp>();
+  addOperations<
+#define GET_OP_LIST
+#include "Ops.cpp.inc"
+      >();
 }
 
+::mlir::Type HDKDialect::parseType(::mlir::DialectAsmParser& parser) const {
+  // TODO
 }
+
+/// Print a type registered to this dialect.
+void HDKDialect::printType(::mlir::Type type, ::mlir::DialectAsmPrinter& os) const {
+  // TODO
+}
+
+}  // namespace hdk
