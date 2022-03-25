59e113cdfcfba15ecd33b20aea8070770c907dcc
Alex Baden <alex.baden@gmail.com>
2022-03-25 13:21:17 -0600

    HDK MLIR Dialect 4/?
    Build + Runtime

--- src/HDK.cpp
+++ src/HDK.cpp
@@ -6,15 +6,25 @@
 #include "MLIRGen.h"
 
 #include "mlir/Dialect.h"
+#include "mlir/InitAllPasses.h"
 
 void mlir_test() {
+  std::cout << "Initializing MLIR" << std::endl;
+  //  mlir::registerAllPasses();
+
+  mlir::DialectRegistry registry;
+  //  registry.insert<hdk::HDKDialect>(); // is loaded below?
+  registry.insert<mlir::StandardOpsDialect>();
+
+  std::cout << "MLIR Initialized" << std::endl;
+
   std::cout << "### Testing MLIR Dialect ###" << std::endl;
 
   hdk::AST::KernelSequence sequence;
   auto kernel = hdk::AST::Kernel();
   sequence.emplace_back(kernel);
 
-  mlir::MLIRContext context;
+  mlir::MLIRContext context(registry);
   // Load our Dialect in this MLIR Context.
   context.getOrLoadDialect<hdk::HDKDialect>();
   auto module = hdk::mlirGen(context, sequence);
