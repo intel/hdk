d26140d48fb675d0d76d61fc84285098fe980844
ienkovich <ilya.enkovich@intel.com>
2022-09-22 17:26:33 -0500

    Remove SQLTypeInfo.
    Signed-off-by: ienkovich <ilya.enkovich@intel.com>

--- omniscidb/IR/Datum.cpp
+++ omniscidb/IR/Datum.cpp
@@ -41,33 +41,6 @@
 #include "Shared/misc.h"
 #include "Shared/sqltypes.h"
 
-std::string SQLTypeInfo::type_name[kSQLTYPE_LAST] = {"NULL",
-                                                     "BOOLEAN",
-                                                     "CHAR",
-                                                     "VARCHAR",
-                                                     "NUMERIC",
-                                                     "DECIMAL",
-                                                     "INTEGER",
-                                                     "SMALLINT",
-                                                     "FLOAT",
-                                                     "DOUBLE",
-                                                     "TIME",
-                                                     "TIMESTAMP",
-                                                     "BIGINT",
-                                                     "TEXT",
-                                                     "DATE",
-                                                     "ARRAY",
-                                                     "INTERVAL_DAY_TIME",
-                                                     "INTERVAL_YEAR_MONTH",
-                                                     "TINYINT",
-                                                     "EVAL_CONTEXT_TYPE",
-                                                     "VOID",
-                                                     "CURSOR",
-                                                     "COLUMN",
-                                                     "COLUMN_LIST"};
-std::string SQLTypeInfo::comp_name[kENCODING_LAST] =
-    {"NONE", "FIXED", "RL", "DIFF", "DICT", "SPARSE", "COMPRESSED", "DAYS"};
-
 namespace {
 // Return decimal_value * 10^dscale
 int64_t convert_decimal_value_to_scale_internal(const int64_t decimal_value,
