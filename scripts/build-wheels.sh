aec49665bffe80da12d26f7f5a90dcfb072d0e82
Alexei Fedotov <alexei.fedotov@gmail.com>
2022-10-14 17:17:46 +0200

    r1

--- scripts/build-wheels.sh
+++ scripts/build-wheels.sh
@@ -13,11 +13,11 @@ function repair_wheel {
 
 # Install a system package required by our library
 yum-config-manager --add-repo=https://mirror.steadfastnet.com/epel/7/x86_64/
-yum install -y atlas-devel blas
+#yum install -y atlas-devel blas
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
-    "${PYBIN}/pip" install -r /io/dev-requirements.txt
+#    "${PYBIN}/pip" install -r /io/dev-requirements.txt
     "${PYBIN}/pip" wheel /io/ --no-deps -w wheelhouse/
 done
 
@@ -29,5 +29,6 @@ done
 # Install packages and test
 for PYBIN in /opt/python/*/bin/; do
     "${PYBIN}/pip" install pyhdk --no-index -f /io/wheelhouse
-    (cd "$HOME"; "${PYBIN}/nosetests" pyhdk)
+#    (cd "$HOME"; "${PYBIN}/nosetests" pyhdk)
 done
+
