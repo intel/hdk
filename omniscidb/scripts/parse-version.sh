8b3fa5155dd9e18bfa9aa5129a2670ab0ec1320f
Petr Kurapov <petr.a.kurapov@intel.com>
2022-10-27 13:59:37 -0500

    Remove some old unused config scripts

--- omniscidb/scripts/parse-version.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -f ../CMakeLists.txt ] ; then
-echo $(cat ../CMakeLists.txt | grep -oP '(?<=set\(MAPD_VERSION_MAJOR ").*(?="\))').$(\
-      cat ../CMakeLists.txt | grep -oP '(?<=set\(MAPD_VERSION_MINOR ").*(?="\))').$(\
-      cat ../CMakeLists.txt | grep -oP '(?<=set\(MAPD_VERSION_PATCH ").*(?="\))')$(\
-      cat ../CMakeLists.txt | grep -oP '(?<=set\(MAPD_VERSION_EXTRA ").*(?="\))')
-fi
