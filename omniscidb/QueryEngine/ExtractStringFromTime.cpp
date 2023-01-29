2cddd9da651d45cc86ca61dcc30ff35755042501
Artem Kroviakov <mr.kroviakov@gmail.com>
2023-01-29 10:24:05 -0600

    format style fix

--- omniscidb/QueryEngine/ExtractStringFromTime.cpp
+++ omniscidb/QueryEngine/ExtractStringFromTime.cpp
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */ 
+ */
 
 #ifndef QUERYENGINE_EXTRACTSTRINGFROMTIME_H
 #define QUERYENGINE_EXTRACTSTRINGFROMTIME_H
