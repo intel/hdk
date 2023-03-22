1dc9f7b7f044d68e8eb30f995be97c37336633e0
Petr Kurapov <petr.kurapov@gmail.com>
2023-03-22 11:03:42 -0500

    Remove redundant indirections

--- omniscidb/QueryEngine/GpuInitGroupsImpl.cpp
+++ omniscidb/QueryEngine/GpuInitGroupsImpl.cpp
@@ -6,6 +6,7 @@
 
 #include "GpuInitGroups.h"
 
+// CUDA versions are implemented in GpuInitGroups.cu
 void init_group_by_buffer_on_device_cuda(int64_t* groups_buffer,
                                          const int64_t* init_vals,
                                          const uint32_t groups_buffer_entry_count,
@@ -17,6 +18,19 @@ void init_group_by_buffer_on_device_cuda(int64_t* groups_buffer,
                                          const size_t block_size_x,
                                          const size_t grid_size_x);
 
+void init_columnar_group_by_buffer_on_device_cuda(
+    int64_t* groups_buffer,
+    const int64_t* init_vals,
+    const uint32_t groups_buffer_entry_count,
+    const uint32_t key_count,
+    const uint32_t agg_col_count,
+    const int8_t* col_sizes,
+    const bool need_padding,
+    const bool keyless,
+    const int8_t key_size,
+    const size_t block_size_x,
+    const size_t grid_size_x);
+
 void init_group_by_buffer_on_device_l0(int64_t* groups_buffer,
                                        const int64_t* init_vals,
                                        const uint32_t groups_buffer_entry_count,
@@ -26,7 +40,18 @@ void init_group_by_buffer_on_device_l0(int64_t* groups_buffer,
                                        const bool keyless,
                                        const int8_t warp_size,
                                        const size_t block_size_x,
-                                       const size_t grid_size_x);
+                                       const size_t grid_size_x) {}
+void init_columnar_group_by_buffer_on_device_l0(int64_t* groups_buffer,
+                                                const int64_t* init_vals,
+                                                const uint32_t groups_buffer_entry_count,
+                                                const uint32_t key_count,
+                                                const uint32_t agg_col_count,
+                                                const int8_t* col_sizes,
+                                                const bool need_padding,
+                                                const bool keyless,
+                                                const int8_t key_size,
+                                                const size_t block_size_x,
+                                                const size_t grid_size_x) {}
 
 void init_group_by_buffer_on_device_impl(int64_t* groups_buffer,
                                          const int64_t* init_vals,
@@ -63,31 +88,6 @@ void init_group_by_buffer_on_device_impl(int64_t* groups_buffer,
                                       grid_size_x);
   }
 }
-
-void init_columnar_group_by_buffer_on_device_cuda(
-    int64_t* groups_buffer,
-    const int64_t* init_vals,
-    const uint32_t groups_buffer_entry_count,
-    const uint32_t key_count,
-    const uint32_t agg_col_count,
-    const int8_t* col_sizes,
-    const bool need_padding,
-    const bool keyless,
-    const int8_t key_size,
-    const size_t block_size_x,
-    const size_t grid_size_x);
-
-void init_columnar_group_by_buffer_on_device_l0(int64_t* groups_buffer,
-                                                const int64_t* init_vals,
-                                                const uint32_t groups_buffer_entry_count,
-                                                const uint32_t key_count,
-                                                const uint32_t agg_col_count,
-                                                const int8_t* col_sizes,
-                                                const bool need_padding,
-                                                const bool keyless,
-                                                const int8_t key_size,
-                                                const size_t block_size_x,
-                                                const size_t grid_size_x);
 
 void init_columnar_group_by_buffer_on_device_impl(
     int64_t* groups_buffer,
@@ -129,30 +129,6 @@ void init_columnar_group_by_buffer_on_device_impl(
   }
 }
 
-void init_group_by_buffer_on_device_l0(int64_t* groups_buffer,
-                                       const int64_t* init_vals,
-                                       const uint32_t groups_buffer_entry_count,
-                                       const uint32_t key_count,
-                                       const uint32_t key_width,
-                                       const uint32_t row_size_quad,
-                                       const bool keyless,
-                                       const int8_t warp_size,
-                                       const size_t block_size_x,
-                                       const size_t grid_size_x) {
-  /*todo*/
-}
-void init_columnar_group_by_buffer_on_device(int64_t* groups_buffer,
-                                             const int64_t* init_vals,
-                                             const uint32_t groups_buffer_entry_count,
-                                             const uint32_t key_count,
-                                             const uint32_t agg_col_count,
-                                             const int8_t* col_sizes,
-                                             const bool need_padding,
-                                             const bool keyless,
-                                             const int8_t key_size,
-                                             const size_t block_size_x,
-                                             const size_t grid_size_x) {}
-
 void init_group_by_buffer_on_device(int64_t* groups_buffer,
                                     const int64_t* init_vals,
                                     const uint32_t groups_buffer_entry_count,
@@ -164,7 +140,19 @@ void init_group_by_buffer_on_device(int64_t* groups_buffer,
                                     const size_t block_size_x,
                                     const size_t grid_size_x,
                                     const GpuMgrPlatform gpu_mgr_platform) {
-  init_group_by_buffer_on_device_impl(groups_buffer,
+  if (gpu_mgr_platform == GpuMgrPlatform::CUDA) {
+    init_group_by_buffer_on_device_cuda(groups_buffer,
+                                        init_vals,
+                                        groups_buffer_entry_count,
+                                        key_count,
+                                        key_width,
+                                        agg_col_count,
+                                        keyless,
+                                        warp_size,
+                                        block_size_x,
+                                        grid_size_x);
+  } else if (gpu_mgr_platform == GpuMgrPlatform::L0) {
+    init_group_by_buffer_on_device_l0(groups_buffer,
                                       init_vals,
                                       groups_buffer_entry_count,
                                       key_count,
@@ -173,8 +161,8 @@ void init_group_by_buffer_on_device(int64_t* groups_buffer,
                                       keyless,
                                       warp_size,
                                       block_size_x,
-                                      grid_size_x,
-                                      gpu_mgr_platform);
+                                      grid_size_x);
+  }
 }
 
 void init_columnar_group_by_buffer_on_device(int64_t* groups_buffer,
@@ -189,7 +177,20 @@ void init_columnar_group_by_buffer_on_device(int64_t* groups_buffer,
                                              const size_t block_size_x,
                                              const size_t grid_size_x,
                                              const GpuMgrPlatform gpu_mgr_platform) {
-  init_columnar_group_by_buffer_on_device_impl(groups_buffer,
+  if (gpu_mgr_platform == GpuMgrPlatform::CUDA) {
+    init_columnar_group_by_buffer_on_device_cuda(groups_buffer,
+                                                 init_vals,
+                                                 groups_buffer_entry_count,
+                                                 key_count,
+                                                 agg_col_count,
+                                                 col_sizes,
+                                                 need_padding,
+                                                 keyless,
+                                                 key_size,
+                                                 block_size_x,
+                                                 grid_size_x);
+  } else if (gpu_mgr_platform == GpuMgrPlatform::L0) {
+    init_columnar_group_by_buffer_on_device_l0(groups_buffer,
                                                init_vals,
                                                groups_buffer_entry_count,
                                                key_count,
@@ -199,6 +200,6 @@ void init_columnar_group_by_buffer_on_device(int64_t* groups_buffer,
                                                keyless,
                                                key_size,
                                                block_size_x,
-                                               grid_size_x,
-                                               gpu_mgr_platform);
+                                               grid_size_x);
+  }
 }
