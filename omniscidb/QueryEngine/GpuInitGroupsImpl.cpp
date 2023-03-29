ed082bfefc91a0667b7fd28591d61c7b6f73262b
Petr Kurapov <petr.a.kurapov@intel.com>
2023-03-29 09:57:39 +0000

    Add dummy implemnetations for cuda group init functions

--- omniscidb/QueryEngine/GpuInitGroupsImpl.cpp
+++ omniscidb/QueryEngine/GpuInitGroupsImpl.cpp
@@ -31,6 +31,36 @@ void init_columnar_group_by_buffer_on_device_cuda(
     const size_t block_size_x,
     const size_t grid_size_x);
 
+#ifndef HAVE_CUDA
+void init_group_by_buffer_on_device_cuda(int64_t* groups_buffer,
+                                         const int64_t* init_vals,
+                                         const uint32_t groups_buffer_entry_count,
+                                         const uint32_t key_count,
+                                         const uint32_t key_width,
+                                         const uint32_t agg_col_count,
+                                         const bool keyless,
+                                         const int8_t warp_size,
+                                         const size_t block_size_x,
+                                         const size_t grid_size_x) {
+  CHECK(false);
+}
+
+void init_columnar_group_by_buffer_on_device_cuda(
+    int64_t* groups_buffer,
+    const int64_t* init_vals,
+    const uint32_t groups_buffer_entry_count,
+    const uint32_t key_count,
+    const uint32_t agg_col_count,
+    const int8_t* col_sizes,
+    const bool need_padding,
+    const bool keyless,
+    const int8_t key_size,
+    const size_t block_size_x,
+    const size_t grid_size_x) {
+  CHECK(false);
+}
+#endif
+
 void init_group_by_buffer_on_device_l0(int64_t* groups_buffer,
                                        const int64_t* init_vals,
                                        const uint32_t groups_buffer_entry_count,
