6e6ce3846d693b325419ef3bc51e04fc00a0c782
ienkovich <ilya.enkovich@intel.com>
2023-04-04 10:41:43 -0500

    Move RowSetMemoryOwner code from QueryEngine.
    Signed-off-by: ienkovich <ilya.enkovich@intel.com>

--- omniscidb/QueryEngine/Execute.cpp
+++ omniscidb/QueryEngine/Execute.cpp
@@ -88,8 +88,6 @@ size_t g_approx_quantile_centroids{300};
 
 size_t g_max_log_length{500};
 
-extern bool g_cache_string_hash;
-
 int const Executor::max_gpu_count;
 
 const int32_t Executor::ERR_SINGLE_VALUE_FOUND_MULTIPLE_VALUES;
@@ -438,28 +436,6 @@ StringDictionaryProxy* Executor::getStringDictionaryProxy(
   const int64_t generation =
       with_generation ? string_dictionary_generations_.getGeneration(dict_id_in) : -1;
   return row_set_mem_owner->getOrAddStringDictProxy(dict_id_in, generation);
-}
-
-StringDictionaryProxy* RowSetMemoryOwner::getOrAddStringDictProxy(
-    const int dict_id_in,
-    const int64_t generation) {
-  const int dict_id{dict_id_in < 0 ? REGULAR_DICT(dict_id_in) : dict_id_in};
-  CHECK(data_provider_);
-  const auto dd = data_provider_->getDictMetadata(dict_id);
-  if (dd) {
-    CHECK(dd->stringDict);
-    CHECK_LE(dd->dictNBits, 32);
-    return addStringDict(dd->stringDict, dict_id, generation);
-  }
-  CHECK_EQ(dict_id, DictRef::literalsDictId);
-  if (!lit_str_dict_proxy_) {
-    DictRef literal_dict_ref(DictRef::invalidDbId, DictRef::literalsDictId);
-    std::shared_ptr<StringDictionary> tsd = std::make_shared<StringDictionary>(
-        literal_dict_ref, "", false, true, g_cache_string_hash);
-    lit_str_dict_proxy_ =
-        std::make_shared<StringDictionaryProxy>(tsd, literal_dict_ref.dictId, 0);
-  }
-  return lit_str_dict_proxy_.get();
 }
 
 const StringDictionaryProxy::IdMap* Executor::getStringProxyTranslationMap(
