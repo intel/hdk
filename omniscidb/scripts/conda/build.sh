8966bf16a39b6cbea109955f0ee2f1282cabb46a
Alexei Fedotov <alexei.fedotov@gmail.com>
2023-04-11 13:31:27 +0200

    Fix cuda (#371)

--- omniscidb/scripts/conda/build.sh
+++ omniscidb/scripts/conda/build.sh
@@ -4,6 +4,10 @@ set -vxe
 cd $(dirname "$0")/../../..
 
 test -z "$CC" || export CUDAFLAGS="--compiler-bindir $CC"
+
+nvcc=$(ls -1 /usr/local/cuda-*/bin/nvcc | head -1)
+export PATH=$(dirname "$nvcc"):$PATH
+
 cmake -B build -S . $@
 cmake --build build --parallel $(nproc)
 
