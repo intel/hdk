b0e4b3ebe6813138bbc5a74f74410dd602a88a41
Laurent Montigny <laurent.montigny@intel.com>
2023-04-17 08:24:29 -0500

    Update GpuSMUtils to enable L0

--- omniscidb/QueryEngine/GpuSharedMemoryUtils.cpp
+++ omniscidb/QueryEngine/GpuSharedMemoryUtils.cpp
@@ -163,6 +163,16 @@ void GpuSharedMemCodeBuilder::codegenReduction(const CompilationOptions& co) {
       "f32:32:32-f64:64:64-v16:16:16-"
       "v32:32:32-v64:64:64-v128:128:128-n16:32:64");
   reduction_code.module->setTargetTriple("nvptx64-nvidia-cuda");
+
+#ifdef HAVE_L0
+  reduction_code.module->setDataLayout(
+      "e-p:64:64:64-i1:8:8-i8:8:8-"
+      "i16:16:16-i32:32:32-i64:64:64-"
+      "f32:32:32-f64:64:64-v16:16:16-"
+      "v32:32:32-v64:64:64-v128:128:128-n16:32:64");
+  reduction_code.module->setTargetTriple("spir64-unknown-unknown");
+#endif
+
   llvm::Linker linker(*module_);
   std::unique_ptr<llvm::Module> owner(reduction_code.module);
   bool link_error = linker.linkInModule(std::move(owner));
