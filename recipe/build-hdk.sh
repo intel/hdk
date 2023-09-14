f9f8f3ea915c7d9a5c2dc2840bca9b2c1b0d2c50
Alex Baden <alexander.baden@intel.com>
2023-09-14 11:30:48 -0500

    Remove openssl dependency and associated code

--- recipe/build-hdk.sh
+++ recipe/build-hdk.sh
@@ -31,7 +31,7 @@ cmake -Wno-dev \
     -DCMAKE_PREFIX_PATH=$PREFIX \
     -DCMAKE_INSTALL_PREFIX=$PREFIX/$INSTALL_BASE \
     -DCMAKE_BUILD_TYPE=Release \
-    -DENABLE_AWS_S3=off -B build -S .
+    -B build -S .
 
 cmake --build build --parallel $(nproc)
 cmake --install build --prefix $PREFIX/$INSTALL_BASE
