765a1c3080e8edb7bcffeee9873e0d64a910dd64
bagrorg <kuldanvit@gmail.com>
2023-11-06 14:39:08 +0100

    Fix review issues and format code

--- omniscidb/QueryEngine/AnalyticalTemplatesExtractor.cpp
+++ omniscidb/QueryEngine/AnalyticalTemplatesExtractor.cpp
@@ -1,3 +1,16 @@
+/*
+    Copyright (c) 2023 Intel Corporation
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+        http://www.apache.org/licenses/LICENSE-2.0
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
 #include "AnalyticalTemplatesExtractor.h"
 #include <limits>
 #include <stdexcept>
@@ -5,67 +18,43 @@
 #include "QueryEngine/CostModel/Measurements.h"
 #include "QueryEngine/RelAlgExecutionUnit.h"
 
+std::vector<costmodel::AnalyticalTemplate> AnalyticalTemplatesExtractor::extractTemplates(
+    const RelAlgExecutionUnit& ra_exe_unit) const {
+  Priority current_priority = std::numeric_limits<Priority>::max();
+  std::vector<costmodel::AnalyticalTemplate> templates;
 
-std::vector<costmodel::AnalyticalTemplate> AnalyticalTemplatesExtractor::extractTemplates(const RelAlgExecutionUnit &ra_exe_unit) const {
-    Priority current_priority = std::numeric_limits<Priority>::max();
-    std::vector<costmodel::AnalyticalTemplate> templates;
-
-    for (auto [priority, templ]: priotities_) {
-        if (priority > current_priority) {
-            return templates;
-        }
-
-        if (templSuits(ra_exe_unit, templ)) {
-            current_priority = priority;
-            templates.push_back(templ);
-        }
+  for (auto [priority, templ] : priotities_) {
+    if (priority > current_priority) {
+      return templates;
     }
 
-    return templates;
+    if (templSuits(ra_exe_unit, templ)) {
+      current_priority = priority;
+      templates.push_back(templ);
+    }
+  }
+
+  return templates;
 }
 
-template <>
-bool AnalyticalTemplatesExtractor::templSuits<costmodel::AnalyticalTemplate::GroupBy>(const RelAlgExecutionUnit &ra_exe_unit) const {
-    return !ra_exe_unit.groupby_exprs.empty();
-}
+bool AnalyticalTemplatesExtractor::templSuits(const RelAlgExecutionUnit& ra_exe_unit,
+                                              costmodel::AnalyticalTemplate templ) const {
+  using costmodel::AnalyticalTemplate;
 
-template <>
-bool AnalyticalTemplatesExtractor::templSuits<costmodel::AnalyticalTemplate::Join>(const RelAlgExecutionUnit &ra_exe_unit) const {
-    return !ra_exe_unit.join_quals.empty();
-}
-
-template <>
-bool AnalyticalTemplatesExtractor::templSuits<costmodel::AnalyticalTemplate::Scan>(const RelAlgExecutionUnit &ra_exe_unit) const {
-    return !ra_exe_unit.quals.empty() || !ra_exe_unit.simple_quals.empty();
-}
-
-template <>
-bool AnalyticalTemplatesExtractor::templSuits<costmodel::AnalyticalTemplate::Sort>(const RelAlgExecutionUnit &ra_exe_unit) const {
-    return !ra_exe_unit.sort_info.order_entries.empty();
-}
-
-template <>
-bool AnalyticalTemplatesExtractor::templSuits<costmodel::AnalyticalTemplate::Reduce>(const RelAlgExecutionUnit &ra_exe_unit) const {
-    // TODO(bagrorg): currently we don't even
-    // use Reduce for cost model
-    return false;
-}
-
-bool AnalyticalTemplatesExtractor::templSuits(const RelAlgExecutionUnit &ra_exe_unit, costmodel::AnalyticalTemplate templ) const {
-    using costmodel::AnalyticalTemplate;
-
-    switch (templ) {
+  switch (templ) {
     case AnalyticalTemplate::GroupBy:
-        return templSuits<AnalyticalTemplate::GroupBy>(ra_exe_unit);
+      return !ra_exe_unit.groupby_exprs.empty();
     case AnalyticalTemplate::Join:
-        return templSuits<AnalyticalTemplate::Join>(ra_exe_unit);
+      return !ra_exe_unit.join_quals.empty();
     case AnalyticalTemplate::Sort:
-        return templSuits<AnalyticalTemplate::Sort>(ra_exe_unit);
+      return !ra_exe_unit.sort_info.order_entries.empty();
     case AnalyticalTemplate::Reduce:
-        return templSuits<AnalyticalTemplate::Reduce>(ra_exe_unit);
+      // TODO(bagrorg): currently we don't even
+      // use Reduce for cost model
+      return false;
     case AnalyticalTemplate::Scan:
-        return templSuits<AnalyticalTemplate::Scan>(ra_exe_unit);
+      return !ra_exe_unit.quals.empty() || !ra_exe_unit.simple_quals.empty();
     default:
-        throw std::runtime_error("Unknown template for check was given");
-    }
+      throw std::runtime_error("Unknown template for check was given");
+  }
 }
